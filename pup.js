e5f8a0b5401b3367e56dcfa6e7540946fa600aef
Detlev <detlev.richter@smail.th-koeln.de>
2026-05-15 14:59:12 +0200

    executablePath bei chromium konfigurierbar

--- pup.js
+++ pup.js
@@ -31,6 +31,7 @@ module.exports = {
     
     const browser = await puppeteer.launch({
       headless: true,
+      executablePath: process.env.CHROME_PATH || undefined,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     const page = await browser.newPage();
